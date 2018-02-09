4bb1eb6d3650ab6f354fd0fb0ecdeae076f7590a
Thomas Foricher <thomas.foricher@gmail.com>
2018-02-09 11:25:11 +0100

    Add docs

---
+++ docs/js/resources.js
@@ -0,0 +1,32 @@
+game.resources = [
+    {
+        "name": "32x32_font",
+        "type": "image",
+        "src": "data/img/font/32x32_font.png"
+    },
+    {
+        "name": "exit",
+        "type": "image",
+        "src": "data/img/new/exit.png"
+    },
+    {
+        "name": "floor",
+        "type": "image",
+        "src": "data/img/new/floor.png"
+    },
+    {
+        "name": "man",
+        "type": "image",
+        "src": "data/img/new/man.png"
+    },
+    {
+        "name": "player_frames",
+        "type": "image",
+        "src": "data/img/new/player_frames.png"
+    },
+    {
+        "name": "wall",
+        "type": "image",
+        "src": "data/img/new/wall.png"
+    }
+];
